f4f564e044b48fac568713d1ccd20dd7b20631ed
Ramlal-atreya <f20261561@pilani.bits-pilani.ac.in>
2026-08-29 02:29:53 +0530

    built basic structure and initialised the matrixs and defined sigmoid

---
+++ neural_net.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+input_size = 3
+hidden_size = 4
+output_size = 2
+
+W1 = np.random.randn(hidden_size, input_size) * 0.01
+b1 = np.zeros((hidden_size, 1))
+W2 = np.random.randn(output_size, hidden_size) * 0.01
+b2 = np.zeros((output_size, 1))
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def neuron_forward(X):
+    Z1 = np.dot(W1, X) + b1
+    A1 = sigmoid(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    return A2
